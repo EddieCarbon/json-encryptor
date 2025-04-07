ae1e78d0ac5416b7a7a5c3d822f13c13f032bef7
Jakub Tomiczek <tomiczekkuba01@gmail.com>
2025-04-07 20:33:40 +0200

    Adds encrypt and decrypt files
    This commit introduces the initial files for encryption and decryption functionality.
    
    These files serve as a starting point for implementing data protection features within the application.

---
+++ JSONEncryptor/decrypt.swift
@@ -0,0 +1,8 @@
+//
+//  decrypt.swift
+//  JSONEncryptor
+//
+//  Created by Jakub Tomiczek on 07/04/2025.
+//
+
+import Foundation
---
+++ JSONEncryptor/encrypt.swift
@@ -0,0 +1,8 @@
+//
+//  encrypt.swift
+//  JSONEncryptor
+//
+//  Created by Jakub Tomiczek on 07/04/2025.
+//
+
+import Foundation
